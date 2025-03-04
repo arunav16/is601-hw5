288a2b0fab7827e6bd282f97a189d97e92f181f6
arunav16 <am3945@njit.edu>
2025-03-03 23:52:27 -0500

    added ENV-VAR test

--- tests/test_app.py
+++ tests/test_app.py
@@ -239,3 +239,10 @@ def test_app_immediate_exit(monkeypatch, capsys):
         App().start()
     output = capsys.readouterr().out
     assert "Type 'exit' to exit." in output, "REPL should prompt exit instructions before exiting."
+
+def test_app_get_environment_variable():
+    app = App()
+#   Retrieve the current environment setting
+    current_env = app.get_environment_variable('ENVIRONMENT')
+    # Assert that the current environment is what you expect
+    assert current_env in ['DEVELOPMENT', 'TESTING', 'PRODUCTION'], f"Invalid ENVIRONMENT: {current_env}"
